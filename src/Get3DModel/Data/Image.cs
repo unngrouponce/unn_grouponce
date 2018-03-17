10bf14a84486c73fc61875abd23051a8443ac6d7
ShatalinaK <shatalina.k@gmail.com>
2018-03-17 18:16:01 +0300

    change project structure

---
+++ src/Get3DModel/Data/Image.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+using System.Text.RegularExpressions;
+using ParsingInputData;
+
+namespace Data
+{
+    /// <summary>
+    /// Класс для преобразованной картинки PNG
+    /// </summary>
+    public class Image
+    {
+        Bitmap image;
+        double height;
+
+        public Image(string pathImage)
+        {
+            IParser parser = new Parser();
+            image = parser.readPNG(pathImage);
+
+            string[] path = pathImage.Split('/');
+            string name = path[path.Length - 1].Replace(".png", "");
+            height = Convert.ToDouble(name);
+        }
+    }
+}
---
+++ src/Get3DModel/Data/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+using System.Text.RegularExpressions;
+using ParsingInputData;
+
+namespace Data
+{
+    /// <summary>
+    /// Класс для полученного решения системы
+    /// </summary>
+    public class Solution
+    {
+        Bitmap sharpImage;
+        double[,] mapHeights;
+    }
+}
