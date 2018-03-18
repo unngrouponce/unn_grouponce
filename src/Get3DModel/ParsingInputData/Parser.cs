23d977110ceefec23eadbabf43e01037396a3a60
Aleksandr Kulikov <unnotigkeit@yandex.ru>
2018-03-18 11:27:03 +0300

    Update Parser.cs

--- src/Get3DModel/ParsingInputData/Parser.cs
+++ src/Get3DModel/ParsingInputData/Parser.cs
@@ -2,20 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+using System.Text.RegularExpressions;
+
 
 namespace ParsingInputData
 {
-    public class Parser:IParser
+   public class Parser:IParser
     {
         public Dictionary<string, double> readConfig(string path)
         {
-            throw new NotImplementedException();
+            Dictionary<string, double> res = new Dictionary<string, double>();
+            string conf = new StreamReader(path).ReadToEnd();
+           var _FDISTANCE = Convert.ToDouble(new Regex(@"f=(\d+)").Match(conf).Groups[1].ToString());
+           var  _FWIDTH = Convert.ToDouble(new Regex(@"w=(\d+)").Match(conf).Groups[1].ToString());
+            res["FDISTANCE"] = _FDISTANCE;
+            res["FWIDTH"] = _FWIDTH;
+            res["HCOEFF"] = 0;
+            return res;
         }
 
         public System.Drawing.Bitmap readPNG(string path)
         {
-            throw new NotImplementedException();
+            return (Bitmap)Image.FromFile(path);
         }
     }
 }
