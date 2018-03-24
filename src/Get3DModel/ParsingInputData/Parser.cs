3d6410585807d50da9ff6266a08f5cbe73e5cc58
ShatalinaK <shatalina.k@gmail.com>
2018-03-24 15:00:22 +0300

    correct project architecture

--- src/Get3DModel/ParsingInputData/Parser.cs
+++ src/Get3DModel/ParsingInputData/Parser.cs
@@ -15,8 +15,8 @@ namespace ParsingInputData
         {
             Dictionary<string, double> res = new Dictionary<string, double>();
             string conf = new StreamReader(path).ReadToEnd();
-           var _FDISTANCE = Convert.ToDouble(new Regex(@"f=(\d+)").Match(conf).Groups[1].ToString());
-           var  _FWIDTH = Convert.ToDouble(new Regex(@"w=(\d+)").Match(conf).Groups[1].ToString());
+            var _FDISTANCE = Convert.ToDouble(new Regex(@"f=(\d+)").Match(conf).Groups[1].ToString());
+            var  _FWIDTH = Convert.ToDouble(new Regex(@"w=(\d+)").Match(conf).Groups[1].ToString());
             res["FDISTANCE"] = _FDISTANCE;
             res["FWIDTH"] = _FWIDTH;
             res["HCOEFF"] = 0;
