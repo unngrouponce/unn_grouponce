f6aaa6b8f8397796fae44d41a564e27c474f1905
MY\Ushakova <ushakova.katia@yande.ru>
2018-04-01 19:53:03 +0300

    Система Юнит тестов+тестирование парсинга

---
+++ src/Get3DModel/Get3DModel.UnitTests/Point3DTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Get3DModel.UnitTests
+{
+    [TestClass]
+    public class Point3DTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ src/Get3DModel/Get3DModel.UnitTests/PreserveOBJTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Get3DModel.UnitTests
+{
+    [TestClass]
+    public class PreserveOBJTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
