2a5b57e3d7051f18364393b79a58b5d415c4040c
ShatalinaK <shatalina.k@gmail.com>
2018-04-23 07:12:43 +0300

    изменения Get3DModel.UnitTests

---
+++ src/Get3DModel/Get3DModel.UnitTests/AnalysisTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Drawing;
+using CalculatedBlock;
+using Data;
+using ParsingInputData;
+
+namespace Get3DModel.UnitTests
+{
+    [TestClass]
+    public class AnalysisTests
+    {
+        [TestMethod]
+        public void getDispersionTest()          //Проверка подсчета дисперсии
+        {
+            //arrange
+            List<double> l = new List<double>() { 0, 1, 5.5, 1};
+            List<Data.Point> listPoint=new List<Data.Point>();
+            Analysis an = new Analysis(listPoint);
+            
+            //act
+            double p=1/l.Count;              //вероятность
+            double M=0;                      //мат ожидание
+             for (int i=0;i<l.Count;i++)
+                M=M+(l[i]*p);
+            double D=0;                      //диспесия
+            for (int i=0;i<l.Count;i++)
+                D=D+Math.Pow((l[i]-M),2)*p;
+
+            double exepected = D;
+            double actual = an.getDispersion(l);
+
+            //assert
+            Assert.AreEqual(exepected, actual);
+        }
+
+        [TestMethod]
+        public void getCore()          //Проверка подсчета дисперсии
+        {
+            //arrange
+            List<double> l = new List<double>() { 0, 1, 5.5, 1 };
+            List<Data.Point> listPoint = new List<Data.Point>();
+            Analysis an = new Analysis(listPoint);
+
+            //act
+            double p = 1 / l.Count;              //вероятность
+            double M = 0;                      //мат ожидание
+            for (int i = 0; i < l.Count; i++)
+                M = M + (l[i] * p);
+            double D = 0;                      //диспесия
+            for (int i = 0; i < l.Count; i++)
+                D = D + Math.Pow((l[i] - M), 2) * p;
+
+            double exepected = D;
+            double actual = an.getDispersion(l);
+
+            //assert
+            Assert.AreEqual(exepected, actual);
+        }
+    }
+}
--- src/Get3DModel/Get3DModel.UnitTests/MathematicialSearchPoint4Test.cs
+++ src/Get3DModel/Get3DModel.UnitTests/MathematicialSearchPoint4Test.cs
@@ -24,8 +24,8 @@ namespace Get3DModel.UnitTests
 
             //assert
             Debug.WriteLine("Проверка ядер  свертки Gx и Gy");
-            for (int i = 1; i < 3; i++)
-                for (int j = 1; j < 3; j++)
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
                     Assert.AreEqual(exepectedG[i, j], actualG[i, j]);
         }
 
